1adfe613231fd147a5b1bb897dcf1f8a7f79a1bc
Karthik Saiharsh <ks.the.visionary@gmail.com>
2026-02-04 22:11:41 +0530

    Task 1.1.3.1, Setup Server Side Form Action logic for New User Registration

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,89 @@
+import { fail, redirect } from "@sveltejs/kit";
+import { db } from "$lib/server/db";
+import { users, userSecrets } from "$lib/server/db/schema";
+import { generateDidWeb } from "$lib/server/didServer";
+import { encrypt } from "$lib/server/encryption";
+import { eq } from "drizzle-orm";
+import z, { success } from "zod";
+import * as argon2 from "argon2";
+import type { Actions } from "./$types";
+
+// Zod Schema for validation
+const registerSchema = z.object({
+    username: z.string()
+        .min(5, { message: "Username must be at least 5 characters" })
+        .regex(/^[a-zA-Z0-9_]+$/, { message: "Username can only contain letters, numbers, and underscores" }),
+    password: z.string()
+        .min(12, { message: "Password must be at least 12 characters" })
+        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+        .regex(/[0-9]/, { message: "Password must contain at least one number" })
+        .regex(/\!\@\#\$\%\^\&\*\-\+/, { message: "Password must contain at least one special character [!,@,#,$,%,^,&,*,-,+]" }),
+});
+
+type UserAuthData = z.infer<typeof registerSchema>;
+
+
+export const actions: Actions = {
+    default: async ({ request }) => {
+        const formData = await request.formData();
+
+        const usernameRaw = formData.get("username");
+        const passwordRaw = formData.get("password");
+
+        // Validate username and password with zod
+        const isValid = registerSchema.safeParse({
+            username: usernameRaw?.toString(),
+            password: passwordRaw?.toString()
+        })
+
+        // Return failure if validation fails
+        if (!isValid.success) {
+            return fail(400, {
+                errors: z.prettifyError(isValid.error)
+            });
+        }
+
+        const validatedUserData: UserAuthData = isValid.data;
+
+        try {
+
+            // Check if user is already existing:
+            const userExisting = await db.select().from(users).where(eq(users.username, validatedUserData.username));
+
+            // Hash Password with Argon2
+            const pswdHash = await argon2.hash(validatedUserData.password);
+
+            // Generate a new DID Identity for this user
+            const didResult = await generateDidWeb(validatedUserData.username);
+
+            // Encrypt Private key
+            const encryptedPrivateKey = encrypt(didResult.privateKey);
+
+            // Add data to db (Transaction)
+            await db.transaction(async (tx) => {
+
+                // Add public viewable user data
+                const [newUser] = await tx.insert(users).values({
+                    username: validatedUserData.username,
+                    passwordHash: pswdHash,
+                    didDocument: didResult.didDocument
+                }).returning({ id: users.id });
+
+                // Add secrets to sepeerate db
+                await tx.insert(userSecrets).values({
+                    userId: newUser.id,
+                    encryptedPrivateKey: encryptedPrivateKey
+                });
+            });
+
+            return {
+                success: true,
+                did: didResult.did
+            }
+
+        } catch (e) {
+            console.log("Error when trying to register new user: ", e);
+            return fail(500, { message: "Internal Server Error" });
+        }
+    }
+}
