52e437bcf33ca6a716e371b347729db3053de834
Karthik Saiharsh <ks.the.visionary@gmail.com>
2026-02-05 23:00:16 +0530

    Task 1.1.3.2 UI for user registration has been implemented

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,10 +1,10 @@
-import { fail, redirect } from "@sveltejs/kit";
+import { fail } from "@sveltejs/kit";
 import { db } from "$lib/server/db";
 import { users, userSecrets } from "$lib/server/db/schema";
 import { generateDidWeb } from "$lib/server/didServer";
 import { encrypt } from "$lib/server/encryption";
 import { eq } from "drizzle-orm";
-import z, { success } from "zod";
+import z from "zod";
 import * as argon2 from "argon2";
 import type { Actions } from "./$types";
 
@@ -17,7 +17,7 @@ const registerSchema = z.object({
         .min(12, { message: "Password must be at least 12 characters" })
         .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
         .regex(/[0-9]/, { message: "Password must contain at least one number" })
-        .regex(/\!\@\#\$\%\^\&\*\-\+/, { message: "Password must contain at least one special character [!,@,#,$,%,^,&,*,-,+]" }),
+        .regex(/[!@#$%^&*\-+]/, { message: "Password must contain at least one special character [!,@,#,$,%,^,&,*,-,+]" }),
 });
 
 type UserAuthData = z.infer<typeof registerSchema>;
@@ -50,6 +50,11 @@ export const actions: Actions = {
             // Check if user is already existing:
             const userExisting = await db.select().from(users).where(eq(users.username, validatedUserData.username));
 
+
+            if (userExisting.length > 0) {
+                return fail(409, { errors: "Username already taken" });
+            }
+
             // Hash Password with Argon2
             const pswdHash = await argon2.hash(validatedUserData.password);
 
@@ -59,21 +64,17 @@ export const actions: Actions = {
             // Encrypt Private key
             const encryptedPrivateKey = encrypt(didResult.privateKey);
 
-            // Add data to db (Transaction)
-            await db.transaction(async (tx) => {
+            // Add public viewable user data
+            const [newUser] = await db.insert(users).values({
+                username: validatedUserData.username,
+                passwordHash: pswdHash,
+                didDocument: didResult.didDocument
+            }).returning({ id: users.id });
 
-                // Add public viewable user data
-                const [newUser] = await tx.insert(users).values({
-                    username: validatedUserData.username,
-                    passwordHash: pswdHash,
-                    didDocument: didResult.didDocument
-                }).returning({ id: users.id });
-
-                // Add secrets to sepeerate db
-                await tx.insert(userSecrets).values({
-                    userId: newUser.id,
-                    encryptedPrivateKey: encryptedPrivateKey
-                });
+            // Add secrets to separate db
+            await db.insert(userSecrets).values({
+                userId: newUser.id,
+                encryptedPrivateKey: encryptedPrivateKey
             });
 
             return {
@@ -83,7 +84,7 @@ export const actions: Actions = {
 
         } catch (e) {
             console.log("Error when trying to register new user: ", e);
-            return fail(500, { message: "Internal Server Error" });
+            return fail(500, { errors: "Internal Server Error" });
         }
     }
 }
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Alert from '$lib/components/ui/alert/index.js';
+
+	import type { PageProps } from './$types';
+
+	let { form }: PageProps = $props();
+</script>
+
+<section class="flex h-screen w-screen items-center justify-center overflow-hidden select-none">
+	{#if form?.errors || form?.success}
+		<section
+			class="absolute flex h-screen w-screen flex-col items-center justify-center bg-background/20 backdrop-blur select-none"
+		>
+			<Alert.Root class={`w-1/3 shadow-[0px_0px_50px_0px_#80000000]`}>
+				<Alert.Title class={`${form?.errors && 'text-red-400'}`}
+					>{form?.success ? 'Error Occurred' : 'Yayy!'}</Alert.Title
+				>
+
+				{#if form?.success}
+					<Alert.Description class="font-bold">Your DID Web Token is:</Alert.Description>
+				{/if}
+
+				<Alert.Description>{form?.errors ? form.errors : form.did}</Alert.Description>
+				<Alert.Description
+					><p class="w-full py-3 text-center text-balance">
+						Refresh page to try again
+					</p></Alert.Description
+				>
+			</Alert.Root>
+		</section>
+	{/if}
+
+	<!-- User Registration Card -->
+	<Card.Root class="-my-4 w-full max-w-sm">
+		<Card.Header>
+			<Card.Title>Register on PolyVerse</Card.Title>
+			<Card.Description
+				>Hello there! Your journey to the future of social media starts here</Card.Description
+			>
+		</Card.Header>
+		<Card.Content>
+			<form id="register-form" method="POST">
+				<div class="flex flex-col gap-6">
+					<div class="grid gap-2">
+						<Label for="username">Username</Label>
+						<Input
+							id="username"
+							name="username"
+							type="text"
+							placeholder="victor_von_doom"
+							required
+						/>
+					</div>
+					<div class="grid gap-2">
+						<div class="flex items-center">
+							<Label for="password">Create a Password</Label>
+						</div>
+						<Input id="password" name="password" type="password" required />
+					</div>
+				</div>
+			</form>
+		</Card.Content>
+		<Card.Footer class="flex-col gap-2">
+			<Button type="submit" form="register-form" class="w-full cursor-pointer"
+				>Join the PolyVerse</Button
+			>
+		</Card.Footer>
+	</Card.Root>
+</section>
